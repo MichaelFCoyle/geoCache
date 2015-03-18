dd62a2cee6eb28f9d5423de1d5636fae79e384f2
Michael Coyle <michael.coyle@bluetoque.ca>
2015-03-17 17:15:38 -0700

    Some missing files added

---
+++ Source/Extensions/geoCache.Caches.Azure/AzureCache.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Diagnostics;
+using GeoCache.Core;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace GeoCache.Caches.Azure
+{
+    public class AzureCache : ICache
+    {
+        public AzureCache()
+            : this("")
+        {
+
+        }
+
+        private AzureCache(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        public string ConnectionString { get; set; }
+
+        CloudStorageAccount StorageAccount { get; set; }
+
+        CloudBlobClient Client { get; set; }
+
+        public byte[] Set(ITile tile, byte[] data)
+        {
+            CloudBlobContainer container = GetContainer(tile.Layer.Name);
+            string name = GetFileName(tile);
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(name);
+            blockBlob.UploadFromByteArray(data, 0, data.Length);
+            return data;
+        }
+
+        public void Lock(ITile tile)
+        {
+        }
+
+        public bool Lock(ITile tile, bool blocking)
+        {
+            return true;
+        }
+
+        public byte[] Get(ITile tile)
+        {
+            try
+            {
+                CloudBlobContainer container = GetContainer(tile.Layer.Name);
+                string name = GetFileName(tile);
+                CloudBlockBlob blockBlob = container.GetBlockBlobReference(name);
+                if (blockBlob.Exists())
+                    return new System.Net.WebClient().DownloadData(blockBlob.Uri.ToString());
+                else
+                    return null;
+            }
+            catch(Exception ex)
+            {
+                Trace.TraceError("AzureCache.Get: Error\r\n{0}", ex);
+                return null;
+            }
+        }
+
+        public void Unlock(ITile tile)
+        {
+        }
+
+        private string GetFileName(ITile tile)
+        {
+            return new XYFileName().GetFileNames(tile);
+        }
+
+        private CloudBlobContainer GetContainer(string containerName)
+        {
+            if (StorageAccount == null)
+                StorageAccount = CloudStorageAccount.Parse(ConnectionString);
+
+            if (Client == null)
+                Client = StorageAccount.CreateCloudBlobClient();
+
+            // Retrieve a reference to a container. 
+            CloudBlobContainer container = Client.GetContainerReference(containerName);
+
+            // Create the container if it doesn't already exist.
+            if (container.CreateIfNotExists())
+            {
+                container.SetPermissions(
+                    new BlobContainerPermissions
+                    {
+                        PublicAccess =
+                            BlobContainerPublicAccessType.Blob
+                    });
+            }
+            return container;
+        }
+    }
+}
